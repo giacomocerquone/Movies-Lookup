febfe2bd2ccbe58c6f8025952bfc1bced64cc42f
Giacomo Cerquone <cerquone@gmail.com>
2015-11-27 04:41:04 +0100

    Corrected bugs, implemented check for callback and renamed main js file

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ var express = require('express'),
 app         = express(),
 fs          = require('fs'),
 omdb        = require('omdb'),
-path        = (if typeof process.argv[2] !== undefined ? process.argv[2] : ""),
-html        = "",
+path        = (typeof process.argv[2] !== 'undefined' ? process.argv[2] : ""),
 Trakt       = require('trakt-api'),
-trakt       = Trakt("1b760a511b5e173f51878ec13ff3ebbc80267502d7d1a541618bde514e81da21");
+trakt       = Trakt("1b760a511b5e173f51878ec13ff3ebbc80267502d7d1a541618bde514e81da21"),
+cbCounter  = 0;
+
 
 if(path == "") {
+
     app.get('/', function (req, res) {
         res.sendFile(__dirname+'/index.html');
     });
     
     app.get('/scan', function(req, res) {
         path = req.query.path;
-    
-        console.log(path);
-    
+            
+            
         fs.readdir(path, function(err, files) {
+            var html = "";
             files.forEach(function(file) {
                 name = file.split(".");
                 name.pop();
-    
-                console.log(name.join());
                 
                 trakt.searchMovie(name.join(), function(err, item) {
                     if(err) return console.warn('oh noes', err);
@@ -32,26 +32,32 @@ if(path == "") {
     
                         tomato = (typeof movie.tomato === 'undefined' ? 'null' : movie.tomato.rating);
                         html += "<p>Title: "+movie.title+"</p><p>IMDB Rating: "+movie.imdb.rating+"</p><p>Tomatoes rating: "+tomato+"</p><hr>";
-                        console.log('Title: '+movie.title);
-                        console.log('IMDB rating: '+movie.imdb.rating);
-                        console.log('Tomatoes rating: '+tomato);
-                        console.log('');
+                        //Increment to check at what point the callback is
+                        cbCounter++;
+                        if(cbCounter == files.length) {
+                            console.log(html);
+                            res.send(html);
+                        }
                     });
                 });
             });
-            res.send(html);
-            html = "";
         });
-    
-    
+        
     });
-    
-    
     
     var server = app.listen(8080, function() {
         console.log("Listening on 8080");
     });
     
 } else {
-    logic here
+
+
+    /*console.log('Title: '+movie.title);
+    console.log('IMDB rating: '+movie.imdb.rating);
+    console.log('Tomatoes rating: '+tomato);
+    console.log('');
+*/
+
+console.log("wrong");
+
 }

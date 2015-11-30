229390f0975bd6e3f4f526fe509b69704973336e
Giacomo Cerquone <cerquone@gmail.com>
2015-11-30 03:02:17 +0100

    Removed completely useless counters, and fixed stuff

--- app.js
+++ app.js
@@ -5,40 +5,41 @@ omdb        = require('omdb'),
 path        = (typeof process.argv[2] !== 'undefined' ? process.argv[2] : ""),
 Trakt       = require('trakt-api'),
 trakt       = Trakt("1b760a511b5e173f51878ec13ff3ebbc80267502d7d1a541618bde514e81da21"),
-cbCounter   = 0,
 ratings     = [];
 
 var getRatings = function(path, cb) {
     fs.readdir(path, function(err, files) {
-        console.log(err);
         // Delete from the array every file that isn't a movie and trim the extension of the movies' filenames
         for(var i = files.length-1;i >= 0;i--) {
             name = files[i].split(".");
             ext = name.pop();
-            if(ext != "mkv")
+            if(ext != "mkv") {
                 files.splice(i, 1);
-            else
+            } else {
                 files[i] = name.join();
+            }
         }
 
-        files.forEach(function(file) {
+        files.forEach(function(file, index) {
             trakt.searchMovie(file, function(err, item) {
-                if(err) return console.warn('Errors with trakt.', err);
-                omdb.get(item[0].movie.ids.imdb, {tomatoes: true}, function(err, movie) {
-                    if(err) return console.error(err);
-                    if(!movie) return console.log('Movie not found!');
-
-                    tomato = (typeof movie.tomato === 'undefined' ? 'null' : movie.tomato.rating);
-                    ratings.push({"title": movie.title, "imdb": movie.imdb.rating, "tomato": tomato});
-
-                    //Increment to check at what point the callback is
-                    cbCounter++;
-                    if(cbCounter == files.length) {
-                        cb(ratings);
-                    }
-                });
+                if(err) console.warn('Errors with trakt.', err);
+                else if(item.length == 0) console.log(file+' not found on Trakt'); 
+                else {
+                    omdb.get(item[0].movie.ids.imdb, {tomatoes: true}, function(err, movie) {
+                        if(err) console.warn('Errors with imdb.', err);
+                        else if(!movie) console.log(file+' not found on IMDB');
+                        else {
+                            tomato = (typeof movie.tomato === 'undefined' ? 'null' : movie.tomato.rating);
+                            ratings.push({"title": movie.title, "imdb": movie.imdb.rating, "tomato": tomato});
+                            
+                        }
+                        if(index == files.length-1) {
+                            cb(ratings);
+                            ratings = [];
+                        } 
+                    });
+                }
             });
-
         });
     });
 };
@@ -52,10 +53,15 @@ if(path == "") {
     app.get('/scan', function(req, res) {
         getRatings(req.query.path, function(ratings) {
             var html = "";
-            ratings.forEach(function(file) {
-                html += "<p>Title: "+file.title+"</p><p>IMDB Rating: "+file.imdb+"</p><p>Tomatoes rating: "+file.tomato+"</p><hr>";
+            ratings.forEach(function(file, index) {
+                if(parseFloat(file.imdb) <= 7.0 || parseFloat(file.tomato) <= 7.0) 
+                    red = "style='color:red;'";
+                else
+                    red = ""; 
+
+                html += "<p>Title: "+file.title+"</p><p "+red+">IMDB Rating: "+file.imdb+"</p><p "+red+">Tomatoes rating: "+file.tomato+"</p><hr>";
+                if(index == ratings.length-1) res.send(html);
             });
-            res.send(html);
         });
     });
     
